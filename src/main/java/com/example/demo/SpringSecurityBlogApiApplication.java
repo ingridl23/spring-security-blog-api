dc2016aea8ea5d3bcbbdda40f49363a2981ae970
ingrid <ingridledesma22@gmail.com>
2026-04-30 12:44:53 -0300

    initial commit

---
+++ src/main/java/com/example/demo/SpringSecurityBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBlogApiApplication.class, args);
+	}
+
+}
