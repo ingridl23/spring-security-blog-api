4365e645f2afc521a2d829906d233a4cf482e1cb
ingrid <ingridledesma22@gmail.com>
2026-04-30 13:25:16 -0300

    agrego en package entity la entidad de Permissions

---
+++ src/main/java/com/ingrid/blogapi/entity/Id.java
@@ -0,0 +1,5 @@
+package com.ingrid.blogapi.entity;
+
+public @interface Id {
+
+}
---
+++ src/main/java/com/ingrid/blogapi/entity/Permission.java
@@ -0,0 +1,23 @@
+package com.ingrid.blogapi.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "permissions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Permission {
+
+	 @Id
+	    @GeneratedValue(strategy = Generated.IDENTITY)
+	    private Long id;
+
+	    @Column(unique = true, nullable = false)
+	    private String name;
+	
+	
+}
